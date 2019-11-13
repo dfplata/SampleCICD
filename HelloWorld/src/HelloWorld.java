6358f3e97c35c21fd0250a4204670e57dcd18cef
dfplata <dfplata_12@yahoo.com>
2019-11-13 19:18:59 +0800

    Getting the latest

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Next is Pipeline!!!");     
+		System.out.println("Will start tomorrow!!!");     		
 	}
 
 }
