1f1138391e3320fa84b2a4e03347463c8834641a
dfplata <dfplata_12@yahoo.com>
2019-11-13 19:36:49 +0800

    Added 'Will start'

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Next is Pipeline!!!");     
-		System.out.println("Tomorrow!!!");		
+		System.out.println("Will start tomorrow!!!");		
 	}
 
 }
