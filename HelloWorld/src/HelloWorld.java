51c4d965cec438cfd486939faff7ee9bd4bd383e
dfplata <dfplata_12@yahoo.com>
2019-11-13 19:52:52 +0800

    Remove 'Will start tomorrow'

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Next is Pipeline!!!");     
-		System.out.println("Will start tomorrow!!!");		
 	}
 
 }
