4dbaf64c2a8efdd6ebcd6c67e973e16cfeffc529
dfplata <dfplata_12@yahoo.com>
2019-11-13 20:34:56 +0800

    Added 'Will start Pipeline tomorrow!!!'

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Next is Pipeline!!!");     
-		System.out.println("Branching has started!!!");     
+		System.out.println("Branching has started!!!"); 
+                System.out.println("Will start Pipeline tomorrow!!!");    
 	}
 
 }
