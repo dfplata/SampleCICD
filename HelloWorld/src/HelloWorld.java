55378b72332588254e536967963546b31f342d26
dfplata <dfplata_12@yahoo.com>
2019-11-14 14:28:39 +0800

    Added a new one in master.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!!");
 		System.out.println("Next is Pipeline!!!");     
 		System.out.println("Branching has started!!!"); 
+                System.out.println("Will start pipeline tomorrow!!!");
                 System.out.println("Already built a pipeline!!!");
 	}
 
